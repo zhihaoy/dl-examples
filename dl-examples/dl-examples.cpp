ee436656a654502ecb4671eefd8b7635dba09b04
Zhihao Yuan <zhihao.yuan@broadcom.com>
2021-10-16 01:42:56 -0700

    chore: import default files

---
+++ dl-examples/dl-examples.cpp
@@ -0,0 +1,12 @@
+﻿// dl-examples.cpp : Defines the entry point for the application.
+//
+
+#include "dl-examples.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ dl-examples/dl-examples.h
@@ -0,0 +1,8 @@
+﻿// dl-examples.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
