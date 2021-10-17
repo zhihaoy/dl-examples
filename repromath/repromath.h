9d444daf5af5a36ef009674007d88ac45a872143
Zhihao Yuan <zhihao.yuan@broadcom.com>
2021-10-17 04:41:01 -0700

    feat: export symbols from repromath

--- repromath/repromath.h
+++ repromath/repromath.h
@@ -1,7 +1,9 @@
 ﻿#pragma once
 
+#include "repromath_export.h"
+
 namespace repromath
 {
-auto ddot(int n, double const *x, double const *y) -> double;
-auto dsum(int n, double const *x) -> double;
+REPROMATH_EXPORT auto ddot(int n, double const *x, double const *y) -> double;
+REPROMATH_EXPORT auto dsum(int n, double const *x) -> double;
 }
