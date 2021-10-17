5b89b096a9467dd78531aa806e8ffa4d08046241
Zhihao Yuan <zhihao.yuan@broadcom.com>
2021-10-17 03:47:01 -0700

    feat: create a basic module for dl

---
+++ repromath/repromath.cpp
@@ -0,0 +1,14 @@
+﻿#include "repromath.h"
+
+#include <algorithm>
+#include <numeric>
+
+auto repromath::ddot(int n, double const *x, double const *y) -> double
+{
+    return std::inner_product(x, x + n, y, 0.);
+}
+
+auto repromath::dsum(int n, double const *x) -> double
+{
+    return std::accumulate(x, x + n, 0.);
+}
---
+++ repromath/repromath.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+namespace repromath
+{
+auto ddot(int n, double const *x, double const *y) -> double;
+auto dsum(int n, double const *x) -> double;
+}
