85c4ce3b4bd3d1559b5e8a14ddb47305700eed8d
Zhihao Yuan <zhihao.yuan@broadcom.com>
2021-10-21 01:55:26 -0700

    chore: add the framework to build plugins

---
+++ whereispython/microsoftstore.cpp
@@ -0,0 +1,1 @@
+#include "whereispython.h"
---
+++ whereispython/whereispython.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <filesystem>
+#include <memory>
+
+namespace whereispython
+{
+
+class installation
+{
+  public:
+    virtual auto executable() -> std::filesystem::path = 0;
+    virtual auto windowed_executable() -> std::filesystem::path = 0;
+    virtual ~installation() = default;
+};
+
+class factory
+{
+  public:
+    virtual auto operator[](wchar_t const *) -> std::unique_ptr<installation> = 0;
+};
+
+}
