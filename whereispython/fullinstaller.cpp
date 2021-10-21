b65a454d2a1bc5bc6f0971cf5efb388ed8648c66
Zhihao Yuan <zhihao.yuan@broadcom.com>
2021-10-21 10:26:10 -0700

    feat: export minimal factory objects

--- whereispython/fullinstaller.cpp
+++ whereispython/fullinstaller.cpp
@@ -1,1 +1,37 @@
 #include "whereispython.h"
+
+#pragma comment(linker, "/export:factory=?instance@@3Vfullinstaller_factory@whereispython@@A")
+
+namespace whereispython
+{
+
+class fullinstaller : public installation
+{
+  public:
+    explicit fullinstaller(wchar_t const *version)
+    {
+    }
+
+    auto executable() -> std::filesystem::path override
+    {
+        return "";
+    }
+
+    auto windowed_executable() -> std::filesystem::path override
+    {
+        return "";
+    }
+};
+
+class fullinstaller_factory : public factory
+{
+  public:
+    virtual auto operator[](wchar_t const *version) -> std::unique_ptr<installation>
+    {
+        return std::make_unique<fullinstaller>(version);
+    }
+};
+
+}
+
+whereispython::fullinstaller_factory instance;
--- whereispython/microsoftstore.cpp
+++ whereispython/microsoftstore.cpp
@@ -1,1 +1,37 @@
 #include "whereispython.h"
+
+#pragma comment(linker, "/export:factory=?instance@@3Vmicrosoftstore_factory@whereispython@@A")
+
+namespace whereispython
+{
+
+class microsoftstore : public installation
+{
+  public:
+    explicit microsoftstore(wchar_t const *version)
+    {
+    }
+
+    auto executable() -> std::filesystem::path override
+    {
+        return "";
+    }
+
+    auto windowed_executable() -> std::filesystem::path override
+    {
+        return "";
+    }
+};
+
+class microsoftstore_factory : public factory
+{
+  public:
+    virtual auto operator[](wchar_t const *version) -> std::unique_ptr<installation>
+    {
+        return std::make_unique<microsoftstore>(version);
+    }
+};
+
+}
+
+whereispython::microsoftstore_factory instance;
